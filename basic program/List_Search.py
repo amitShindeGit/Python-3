c18528ab025df07483d2cc8e62054dc875994efa
amitShindeGit <99amitshinde@gmail.com>
2020-11-07 21:11:34 +0530

    Modified List_Search Program

--- basic program/List_Search.py
+++ basic program/List_Search.py
@@ -5,16 +5,19 @@
 @author: saurav
 """
 
-cityname = [] 
+cityname = []
 for i in range(0,5):
-    city=input()
+    city=input('Enter 5 Elements: ')
     cityname.append(city)
 print(cityname)  
-srch = input()
+srch = input('Eneter element to find:  ')
 for i in range(0,5):
+    if (srch == cityname[i]):
+        print("Found element at position:",i+1)
+        break
+    elif (srch != cityname[i]):
+        print('Not Found')
+        break
+        
     
-    if srch==cityname[i]:
-        print("found element at position:",i+1)
-    else:
-        print("Element not found")
     
